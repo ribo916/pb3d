58e24242707d57cb0e379ffb54703641393301f6
ribo916 <richard.boesch@gmail.com>
2026-07-07 20:11:21 -0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+
+const resolvePath = (p) => fileURLToPath(new URL(p, import.meta.url));
 
 export default defineConfig({
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolvePath('./index.html'),
+        characterPreview: resolvePath('./character-preview/index.html')
+      }
+    }
   }
 });
